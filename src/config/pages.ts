0a1361f0e6b00d9ee718b3bfcaa139f9ef7838af
Richard Buckley <42151145+rhbuckley@users.noreply.github.com>
2024-03-23 23:16:22 -0400

    feat: add pages config

---
+++ src/config/pages.ts
@@ -0,0 +1,25 @@
+export const Pages = [
+    {
+        href: "/docs",
+        name: "Docs",
+        description:
+            "Learn more about the LC3 Assembly Language. It is recommended to start here if you are new to the LC3.",
+    },
+    {
+        href: "/learn",
+        name: "Learn",
+        description:
+            "See examples of LC3 code and learn how to write your own.",
+    },
+    {
+        href: "/simulate",
+        name: "Simulate",
+        description:
+            "Simulate LC3 code and see how the registers and memory change.",
+    },
+    {
+        href: "/edu",
+        name: "Education",
+        description: "Enroll in a course, or create a class for your students.",
+    },
+];
